d08d631bd1479b3997115387ebf4916788d2e959
Mario Sergio <marito@Marito-Macbook-Pro.hsd1.fl.comcast.net>
2024-04-20 15:35:21 -0400

    first commit

---
+++ src/editor.rs
@@ -0,0 +1,69 @@
+use std::io::{stdin, stdout, Error};
+use termion::event::Key;
+use termion::input::TermRead;
+// use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {
+    pub should_quit: bool,
+}
+
+
+impl Editor {
+
+    pub fn run(&mut self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.process_keyress() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+        }
+        // for key in io::stdin().keys(){
+        //     match key {
+        //         Ok(key) => match key {
+        //             Key::Char(c)=>{
+        //                 if c.is_control(){
+        //                     println!("first if: {:?}\r", c as u8);
+        //                 }else {
+        //                     println!("the else {:?} ({})\r", c as u8,c);
+
+        //                 }
+        //             }
+        //             Key::Ctrl('c') => break,
+        //             // Key::Alt('c') => break,
+        //             _ => println!("{key:?}\r"),
+        //         }
+        //         Err(err) => die(err),
+        //     }
+        // }
+    }
+
+    fn process_keyress(&mut self) -> Result<(), Error>{
+        let pressed_key = read_key()?;
+        match pressed_key {
+            // Key::Ctrl('q') => panic!("Program end"),
+            Key::Ctrl('c') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+    pub fn default() -> Self {
+        Self{ should_quit: false}
+    }
+
+}
+
+fn read_key() -> Result<Key, Error> {
+    loop {
+        if let Some(key) = stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+fn die(e: Error){
+    panic!("{}",e);
+}
